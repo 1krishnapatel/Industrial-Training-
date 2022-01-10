35bebd3ee88e8f88e063b7e2fba9ee0adf272b8a
Krishna patel <89310602+1krishnapatel@users.noreply.github.com>
2022-01-10 10:12:28 +0530

    Create pyException.py

---
+++ pyException.py
@@ -0,0 +1,9 @@
+a=10
+b=0
+#print("Division Result : ",a/b)
+try:
+    print("Division Reult : ",a/b)
+except:
+    print("divided by number 0 is not allowed")
+
+print("\n message after exception")
