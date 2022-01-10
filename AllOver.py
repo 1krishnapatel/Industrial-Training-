3cf8384f1fcffc9ed92e679184e571eb846caa49
Krishna patel <89310602+1krishnapatel@users.noreply.github.com>
2022-01-10 10:08:26 +0530

    Create AllOver.py

---
+++ AllOver.py
@@ -0,0 +1,28 @@
+p,q,r = 5,15,25
+
+# Arithmetic Operator
+print("\n 5 + 15 = ",p+q)
+
+print("\n 25 - 15 = ",r-q)
+
+print("\n 5 * 15 = ",p*q)
+
+print("\n 25 / 5 = ",r/q)
+
+# Comparison Operator
+x,y = 11,22
+print("\n x < y",x<y)
+
+print("\n x > y",x>y)
+
+print("\n x <= y",x<=y)
+
+print("\n x >= y",x>=y)
+
+print("\n x == y",x==y)
+
+print("\n x != y",x!=y)
+
+# Logical Operator
+x = True
+y = False
